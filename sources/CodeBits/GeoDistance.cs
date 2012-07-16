e2cc3d8592c16c87a57edd6293e4ef6d4c655df5
Jeevan James <JeevanJames@users.noreply.github.com>
2012-07-16 20:33:03 +0000

    Added a new code bit for random password generation called PasswordGenerator. Completed work on the OrderedObservableCollection<T> class. Added nuspecs for PasswordGenerator and OrderedObservableCollection<T>. Added a new class for calculating geographic distances.

---
+++ sources/CodeBits/GeoDistance.cs
@@ -0,0 +1,65 @@
+﻿#region --- License & Copyright Notice ---
+/*
+CodeBits Code Snippets
+Copyright (c) 2012 Jeevan James
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace CodeBits
+{
+    /// <summary>
+    /// Calculates the distance between two geographics locations
+    /// </summary>
+    /// <remarks>
+    /// If you are using .NET 4.0+ or Windows Phone, then this functionality is available through the
+    /// GeoCoordinate.GetDistanceTo method.
+    /// </remarks>
+    public static class GeoDistance
+    {
+        public static double Between(double latitude1, double longitude1, double latitude2, double longitude2,
+            DistanceUnits units = DistanceUnits.Kilometers)
+        {
+            double a1 = latitude1 * RadianConversionFactor;
+            double b1 = longitude1 * RadianConversionFactor;
+            double a2 = latitude2 * RadianConversionFactor;
+            double b2 = longitude2 * RadianConversionFactor;
+
+            double distance =
+                Math.Acos(Math.Cos(a1) * Math.Cos(b1) * Math.Cos(a2) * Math.Cos(b2) +
+                    Math.Cos(a1) * Math.Sin(b1) * Math.Cos(a2) * Math.Sin(b2) + Math.Sin(a1) * Math.Sin(a2)) * DistanceConstants[units];
+
+            return distance;
+        }
+
+        private const double RadianConversionFactor = Math.PI / 180;
+
+        private static readonly Dictionary<DistanceUnits, double> DistanceConstants = new Dictionary<DistanceUnits, double>(3) {
+            { DistanceUnits.Miles, 3963.1 },
+            { DistanceUnits.NauticalMiles, 3443.9 },
+            { DistanceUnits.Kilometers, 6378 }
+        };
+    }
+
+    public enum DistanceUnits
+    {
+        Miles,
+        NauticalMiles,
+        Kilometers,
+    }
+}
---
+++ sources/CodeBits/PasswordGenerator.cs
@@ -0,0 +1,97 @@
+﻿#region --- License & Copyright Notice ---
+/*
+CodeBits Code Snippets
+Copyright (c) 2012 Jeevan James
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CodeBits
+{
+    public static class PasswordGenerator
+    {
+        public static string Generate(int length, PasswordCharacters allowedCharacters = PasswordCharacters.All)
+        {
+            if (length <= 0)
+                throw new ArgumentOutOfRangeException("length", "Password length must be greater than zero");
+
+            var randomBytes = new byte[length];
+            var randomNumberGenerator = new RNGCryptoServiceProvider();
+            randomNumberGenerator.GetBytes(randomBytes);
+
+            string allowedCharactersString = GenerateAllowedCharactersString(allowedCharacters);
+            int allowedCharactersCount = allowedCharactersString.Length;
+
+            var characters = new char[length];
+            for (int i = 0; i < length; i++)
+                characters[i] = allowedCharactersString[randomBytes[i] % allowedCharactersCount];
+            return new string(characters);
+        }
+
+        public static SecureString GenerateSecure(int length, PasswordCharacters allowedCharacters = PasswordCharacters.All)
+        {
+            if (length <= 0)
+                throw new ArgumentOutOfRangeException("length", "Password length must be greater than zero");
+
+            var randomBytes = new byte[length];
+            var randomNumberGenerator = new RNGCryptoServiceProvider();
+            randomNumberGenerator.GetBytes(randomBytes);
+
+            string allowedCharactersString = GenerateAllowedCharactersString(allowedCharacters);
+            int allowedCharactersCount = allowedCharactersString.Length;
+
+            var password = new SecureString();
+            for (int i = 0; i < length; i++)
+                password.AppendChar(allowedCharactersString[randomBytes[i] % allowedCharactersCount]);
+            password.MakeReadOnly();
+            return password;
+        }
+
+        private static string GenerateAllowedCharactersString(PasswordCharacters characters)
+        {
+            var allowedCharactersString = new StringBuilder();
+            foreach (KeyValuePair<PasswordCharacters, string> type in AllowedPasswordCharacters)
+            {
+                if ((characters & type.Key) == type.Key)
+                    allowedCharactersString.Append(type.Value);
+            }
+            return allowedCharactersString.ToString();
+        }
+
+        private static readonly Dictionary<PasswordCharacters, string> AllowedPasswordCharacters = new Dictionary<PasswordCharacters, string>(4) {
+            { PasswordCharacters.LowercaseCharacters, "abcdefghijklmnopqrstuvwxyz" },
+            { PasswordCharacters.UppercaseCharacters, "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+            { PasswordCharacters.Numbers, "0123456789" },
+            { PasswordCharacters.Punctuations, @"~`!@#$%^&*()_-+= {[}]|\:;""'<,>.?/" },
+        };
+    }
+
+    [Flags]
+    public enum PasswordCharacters
+    {
+        LowercaseCharacters = 0x01,
+        UppercaseCharacters = 0x02,
+        Numbers = 0x04,
+        Punctuations = 0x08,
+        AllCharacters = LowercaseCharacters | UppercaseCharacters,
+        All = AllCharacters | Numbers | Punctuations,
+    }
+}
