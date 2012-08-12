0dd6db70e4219be68792163530877f150f959112
Jeevan James <JeevanJames@users.noreply.github.com>
2012-08-12 12:30:23 +0000

    Made IniFileSection and IniFileProperty inner classes in IniFile and renamed them to Section and Property respectively.

--- sources/CodeBits/ListExtensions.cs
+++ sources/CodeBits/ListExtensions.cs
@@ -85,6 +85,21 @@ namespace CodeBits
             }
         }
 
+        public static int LastIndexOf<T>(this IList<T> list, Func<T, bool> predicate)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            if (predicate == null)
+                throw new ArgumentNullException("predicate");
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                if (predicate(list[i]))
+                    return i;
+            }
+            return -1;
+        }
+
         public static bool Remove<T>(this IList<T> list, Func<T, bool> predicate)
         {
             if (list == null)
