edc98f48506841f3a61a37f8bc945d04178e0f45
Jeevan James <JeevanJames@users.noreply.github.com>
2012-10-26 22:51:27 +0000

    Updated the PasswordGenerator's Generate methods with an optional parameter that allows you to specify the characters to exclude from the allowed characters. Suggestion came from MaximRouiller in the CodePlex discussion area (#400918).

--- sources/CodeBits/PasswordGenerator.cs
+++ sources/CodeBits/PasswordGenerator.cs
@@ -22,15 +22,17 @@ limitations under the License.
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace CodeBits
 {
-    public static partial class PasswordGenerator
+    public static class PasswordGenerator
     {
-        public static string Generate(int length, PasswordCharacters allowedCharacters = PasswordCharacters.All)
+        public static string Generate(int length, PasswordCharacters allowedCharacters = PasswordCharacters.All,
+            IEnumerable<char> excludeCharacters = null)
         {
             if (length <= 0)
                 throw new ArgumentOutOfRangeException("length", "Password length must be greater than zero");
@@ -39,7 +41,7 @@ namespace CodeBits
             var randomNumberGenerator = new RNGCryptoServiceProvider();
             randomNumberGenerator.GetBytes(randomBytes);
 
-            string allowedCharactersString = GenerateAllowedCharactersString(allowedCharacters);
+            string allowedCharactersString = GenerateAllowedCharactersString(allowedCharacters, excludeCharacters);
             int allowedCharactersCount = allowedCharactersString.Length;
 
             var characters = new char[length];
@@ -48,7 +50,8 @@ namespace CodeBits
             return new string(characters);
         }
 
-        public static SecureString GenerateSecure(int length, PasswordCharacters allowedCharacters = PasswordCharacters.All)
+        public static SecureString GenerateSecure(int length, PasswordCharacters allowedCharacters = PasswordCharacters.All,
+            IEnumerable<char> excludedCharacters = null)
         {
             if (length <= 0)
                 throw new ArgumentOutOfRangeException("length", "Password length must be greater than zero");
@@ -57,7 +60,7 @@ namespace CodeBits
             var randomNumberGenerator = new RNGCryptoServiceProvider();
             randomNumberGenerator.GetBytes(randomBytes);
 
-            string allowedCharactersString = GenerateAllowedCharactersString(allowedCharacters);
+            string allowedCharactersString = GenerateAllowedCharactersString(allowedCharacters, excludedCharacters);
             int allowedCharactersCount = allowedCharactersString.Length;
 
             var password = new SecureString();
@@ -67,24 +70,29 @@ namespace CodeBits
             return password;
         }
 
-        private static string GenerateAllowedCharactersString(PasswordCharacters characters)
+        private static string GenerateAllowedCharactersString(PasswordCharacters characters, IEnumerable<char> excludeCharacters)
         {
             var allowedCharactersString = new StringBuilder();
             foreach (KeyValuePair<PasswordCharacters, string> type in AllowedPasswordCharacters)
             {
-                if ((characters & type.Key) == type.Key)
+                if ((characters & type.Key) != type.Key)
+                    continue;
+                if (excludeCharacters == null)
                     allowedCharactersString.Append(type.Value);
+                else
+                    allowedCharactersString.Append(type.Value.Where(c => !excludeCharacters.Contains(c)).ToArray());
             }
             return allowedCharactersString.ToString();
         }
 
-        private static readonly Dictionary<PasswordCharacters, string> AllowedPasswordCharacters = new Dictionary<PasswordCharacters, string>(4) {
-            { PasswordCharacters.LowercaseLetters, "abcdefghijklmnopqrstuvwxyz" },
-            { PasswordCharacters.UppercaseLetters, "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
-            { PasswordCharacters.Numbers, "0123456789" },
-            { PasswordCharacters.Punctuations, @"~`!@#$%^&*()_-+={[}]|\:;""'<,>.?/" },
-            { PasswordCharacters.Space, " " },
-        };
+        private static readonly Dictionary<PasswordCharacters, string> AllowedPasswordCharacters =
+            new Dictionary<PasswordCharacters, string>(4) {
+                { PasswordCharacters.LowercaseLetters, "abcdefghijklmnopqrstuvwxyz" },
+                { PasswordCharacters.UppercaseLetters, "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+                { PasswordCharacters.Numbers, "0123456789" },
+                { PasswordCharacters.Punctuations, @"~`!@#$%^&*()_-+={[}]|\:;""'<,>.?/" },
+                { PasswordCharacters.Space, " " },
+            };
     }
 
     [Flags]
