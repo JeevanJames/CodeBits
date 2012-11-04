e11ed50d361897db4b8a07030f79e4ff3ba842b8
jeevanjj_cp <SND\jeevanjj_cp>
2012-11-04 21:09:24 +0000

    Updated the WindowsServiceRunner to 1.3: Fixed issue where running multiple services would run them in sequence. This has necessitated a breaking change to the API, where we first register the services using a new Register method and then run them using the Run method. Also, added a new shortcut constructor and shortcut static method. Updated the events to accept the service type and service instance as event args parameters. Updated the NuGet package.

--- sources/CodeBits/WindowsServiceRunner.cs
+++ sources/CodeBits/WindowsServiceRunner.cs
@@ -25,115 +25,195 @@ Documentation: http://codebits.codeplex.com/wikipage?title=WindowsServiceRunner
 */
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Reflection;
 using System.ServiceProcess;
+using System.Threading.Tasks;
 
 namespace CodeBits
 {
     /// <summary>
-    /// Provides functionality to execute a Windows service class as either a console application or
-    /// a Windows service, depending on how the project properties are set.
-    /// To run as a console application, set the project output type to "Console Application"
-    /// To run as a Windows service, set the project output type to "Windows Application"
+    ///     Provides functionality to execute a Windows service class as either a console application or
+    ///     a Windows service, depending on how the project properties are set.
+    ///     To run as a console application, set the project output type to "Console Application"
+    ///     To run as a Windows service, set the project output type to "Windows Application"
     /// </summary>
-    public sealed partial class WindowsServiceRunner
+    public sealed class WindowsServiceRunner
     {
+        private readonly List<Func<ServiceBase>> _serviceFactories = new List<Func<ServiceBase>>();
+
         static WindowsServiceRunner()
         {
             if (Environment.UserInteractive)
                 Trace.Listeners.Add(new ConsoleTraceListener());
         }
 
-        /// <summary>
-        /// Starts the service logic in the appropriate environment
-        /// </summary>
-        /// <typeparam name="TService">The type of the service to start</typeparam>
-         public void Run<TService>()
-             where TService : ServiceBase, new()
-         {
-             var service = new TService();
-             if (Environment.UserInteractive)
-                 RunAsConsole(service);
-             else
-                 RunAsService(service);
-         }
+        public WindowsServiceRunner()
+        {
+        }
+
+        public WindowsServiceRunner(IEnumerable<Type> serviceTypes)
+        {
+            foreach (Type serviceType in serviceTypes)
+                Register(serviceType);
+        }
+
+        public static void Run(params Type[] serviceTypes)
+        {
+            var runner = new WindowsServiceRunner(serviceTypes);
+            runner.Run();
+        }
 
         /// <summary>
-        /// Fired when the service logic starts in console mode.
+        ///     Registers the specified service type to be run.
         /// </summary>
-        public event EventHandler ConsoleModeStarted;
+        /// <typeparam name="TService"> The type of service to run. This type must be a subclass of ServiceBase and should have a default constructor </typeparam>
+        public void Register<TService>() where TService : ServiceBase, new()
+        {
+            _serviceFactories.Add(() => new TService());
+        }
 
         /// <summary>
-        /// Fired if an exception is thrown in console mode.
+        /// Registers the specified service type to run
+        /// </summary>
+        /// <typeparam name="TService"></typeparam>
+        /// <param name="args"></param>
+        public void Register<TService>(params object[] args) where TService : ServiceBase
+        {
+            _serviceFactories.Add(() => (ServiceBase)Activator.CreateInstance(typeof(TService), args));
+        }
+
+        public void Register(Type serviceType, params object[] args)
+        {
+            if (serviceType == null)
+                throw new ArgumentNullException("serviceType");
+            if (!serviceType.IsSubclassOf(typeof(ServiceBase)))
+            {
+                throw new ArgumentException(
+                    string.Format("Specified service type '{0}' does not subclass System.ServiceProcess.ServiceBase", serviceType),
+                    "serviceType");
+            }
+
+            _serviceFactories.Add(() => (ServiceBase)Activator.CreateInstance(serviceType, args));
+        }
+
+        /// <summary>
+        ///     Starts the services' logic in the appropriate environment.
+        /// </summary>
+        public void Run()
+        {
+            if (_serviceFactories.Count == 0)
+                throw new InvalidOperationException("No services available to run. Add services using the Register method.");
+            if (Environment.UserInteractive)
+                RunAsConsole();
+            else
+                RunAsService();
+        }
+
+        /// <summary>
+        ///     Fired when the service logic starts in console mode.
+        /// </summary>
+        public event EventHandler<ConsoleModeEventArgs> ConsoleModeStarted;
+
+        /// <summary>
+        ///     Fired if an exception is thrown in console mode.
         /// </summary>
         public event EventHandler<ConsoleModeExceptionEventArgs> ConsoleModeException;
 
         /// <summary>
-        /// Fired when the service logic completes in console mode.
+        ///     Fired when the service logic completes in console mode.
         /// </summary>
-        public event EventHandler ConsoleModeStopped;
+        public event EventHandler<ConsoleModeEventArgs> ConsoleModeStopped;
 
-        private void RunAsConsole(ServiceBase service)
+        private void RunAsConsole()
         {
-            Type serviceHostType = service.GetType();
-            MethodInfo onStartMethod = serviceHostType.GetMethod("OnStart", BindingFlags.NonPublic | BindingFlags.Instance);
-            MethodInfo onStopMethod = serviceHostType.GetMethod("OnStop", BindingFlags.NonPublic | BindingFlags.Instance);
+            Parallel.ForEach(_serviceFactories, factory => {
+                ServiceBase service = factory();
+                Type serviceHostType = service.GetType();
+                MethodInfo onStartMethod = serviceHostType.GetMethod("OnStart", BindingFlags.NonPublic | BindingFlags.Instance);
+                MethodInfo onStopMethod = serviceHostType.GetMethod("OnStop", BindingFlags.NonPublic | BindingFlags.Instance);
 
-            try
-            {
-                onStartMethod.Invoke(service, new object[] { null });
-                FireConsoleModeStarted();
-                Console.ReadLine();
-            }
-            catch (Exception ex)
-            {
-                FireConsoleModeException(ex);
-                Console.ReadLine();
-            }
-            finally
-            {
-                onStopMethod.Invoke(service, null);
-                FireConsoleModeStopped();
-            }
+                try
+                {
+                    FireConsoleModeStarted(service, serviceHostType);
+                    onStartMethod.Invoke(service, new object[] { null });
+                }
+                catch (Exception ex)
+                {
+                    FireConsoleModeException(service, serviceHostType, ex);
+                }
+                finally
+                {
+                    onStopMethod.Invoke(service, null);
+                    FireConsoleModeStopped(service, serviceHostType);
+                }
+            });
+            Console.ReadLine();
         }
 
-        private void FireConsoleModeStarted()
+        private void FireConsoleModeStarted(ServiceBase serviceInstance, Type serviceType)
         {
-            EventHandler consoleModeStarted = ConsoleModeStarted;
+            EventHandler<ConsoleModeEventArgs> consoleModeStarted = ConsoleModeStarted;
             if (consoleModeStarted != null)
-                consoleModeStarted(this, EventArgs.Empty);
+                consoleModeStarted(this, new ConsoleModeEventArgs(serviceInstance, serviceType));
         }
 
-        private void FireConsoleModeException(Exception exception)
+        private void FireConsoleModeException(ServiceBase serviceInstance, Type serviceType, Exception exception)
         {
             EventHandler<ConsoleModeExceptionEventArgs> consoleModeException = ConsoleModeException;
             if (consoleModeException != null)
-                consoleModeException(this, new ConsoleModeExceptionEventArgs(exception));
+                consoleModeException(this, new ConsoleModeExceptionEventArgs(serviceInstance, serviceType, exception));
         }
 
-        private void FireConsoleModeStopped()
+        private void FireConsoleModeStopped(ServiceBase serviceInstance, Type serviceType)
         {
-            EventHandler consoleModeStopped = ConsoleModeStopped;
+            EventHandler<ConsoleModeEventArgs> consoleModeStopped = ConsoleModeStopped;
             if (consoleModeStopped != null)
-                consoleModeStopped(this, EventArgs.Empty);
+                consoleModeStopped(this, new ConsoleModeEventArgs(serviceInstance, serviceType));
         }
 
-        private static void RunAsService(ServiceBase service)
+        private void RunAsService()
         {
-            var servicesToRun = new[] { service };
-            ServiceBase.Run(servicesToRun);
+            ServiceBase.Run(_serviceFactories.Select(func => func()).ToArray());
         }
     }
 
     /// <summary>
-    /// Provides data for the ConsoleModeException event
+    ///     Provides data for the console mode lifecycle events.
     /// </summary>
-    public sealed partial class ConsoleModeExceptionEventArgs : EventArgs
+    public class ConsoleModeEventArgs : EventArgs
+    {
+        private readonly ServiceBase _serviceInstance;
+        private readonly Type _serviceType;
+
+        internal ConsoleModeEventArgs(ServiceBase serviceInstance, Type serviceType)
+        {
+            _serviceInstance = serviceInstance;
+            _serviceType = serviceType;
+        }
+
+        public ServiceBase ServiceInstance
+        {
+            get { return _serviceInstance; }
+        }
+
+        public Type ServiceType
+        {
+            get { return _serviceType; }
+        }
+    }
+
+    /// <summary>
+    ///     Provides data for the ConsoleModeException event
+    /// </summary>
+    public sealed class ConsoleModeExceptionEventArgs : ConsoleModeEventArgs
     {
         private readonly Exception _exception;
 
-        internal ConsoleModeExceptionEventArgs(Exception exception)
+        internal ConsoleModeExceptionEventArgs(ServiceBase serviceInstance, Type serviceType, Exception exception)
+            : base(serviceInstance, serviceType)
         {
             if (exception == null)
                 throw new ArgumentNullException("exception");
@@ -141,7 +221,7 @@ namespace CodeBits
         }
 
         /// <summary>
-        /// The exception that was thrown.
+        ///     The exception that was thrown.
         /// </summary>
         public Exception Exception
         {
