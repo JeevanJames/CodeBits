a5c2b7ae8c90a19bf5ef0cb7f2e7893f5fc12ca5
Jeevan James <jeevanjj@live.com>
2017-10-17 01:57:32 +0530

    Added project for .NET 3.5 and C# 3 compatible code

---
+++ sources/CodeBits_Net30/ByteSizeFriendlyName.cs
@@ -0,0 +1,133 @@
+﻿#region --- License & Copyright Notice ---
+/*
+CodeBits Code Snippets
+Copyright (c) 2012-2017 Jeevan James
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#endregion
+
+/* Documentation: https://github.com/JeevanJames/CodeBits/wiki/ByteSizeFriendlyName */
+
+using System;
+using System.Collections.Generic;
+
+namespace CodeBits
+{
+    public static partial class ByteSizeFriendlyName
+    {
+        public static string Build(long bytes)
+        {
+            return Build(bytes, null);
+        }
+
+        public static string Build(long bytes, FriendlyNameOptions options)
+        {
+            if (bytes < 0)
+                throw new ArgumentOutOfRangeException("bytes", "bytes cannot be a negative value");
+
+            options = options ?? FriendlyNameOptions.Default;
+
+            string units;
+            double friendlySize = GetFriendlySize(bytes, out units);
+
+            string sizeFormat = (options.GroupDigits ? "#,0." : "0.") + new string('#', options.DecimalPlaces);
+            string size = friendlySize.ToString(sizeFormat);
+
+            if (options.UnitDisplayMode == UnitDisplayMode.AlwaysDisplay)
+                return string.Format("{0} {1}", size, units);
+            if (options.UnitDisplayMode == UnitDisplayMode.AlwaysHide)
+                return size;
+            return bytes < 1024 ? size : string.Format("{0} {1}", size, units);
+        }
+
+        private static double GetFriendlySize(long bytes, out string units)
+        {
+            foreach (KeyValuePair<double, string> byteMapping in ByteMappings)
+            {
+                if (bytes >= byteMapping.Key)
+                {
+                    units = byteMapping.Value;
+                    return bytes / byteMapping.Key;
+                }
+            }
+            units = bytes == 1 ? "byte" : "bytes";
+            return bytes;
+        }
+
+        private static readonly Dictionary<double, string> ByteMappings = new Dictionary<double, string> {
+            { Math.Pow(1024, 5), "PB" },
+            { Math.Pow(1024, 4), "TB" },
+            { Math.Pow(1024, 3), "GB" },
+            { Math.Pow(1024, 2), "MB" },
+            { Math.Pow(1024, 1), "KB" },
+        };
+    }
+
+    /// <summary>
+    /// Options for generating the friendly name of a byte size.
+    /// </summary>
+    public sealed class FriendlyNameOptions
+    {
+        /// <summary>
+        /// Creates an instance of the FriendlyNameOptions class.
+        /// </summary>
+        public FriendlyNameOptions()
+        {
+            DecimalPlaces = 2;
+            UnitDisplayMode = UnitDisplayMode.AlwaysDisplay;
+        }
+
+        /// <summary>
+        /// The number of decimal places to calculate for the friendly name size value.
+        /// </summary>
+        public int DecimalPlaces { get; set; }
+
+        /// <summary>
+        /// Specifies whether to group digits in the friendly name size value.
+        /// </summary>
+        public bool GroupDigits { get; set; }
+
+        /// <summary>
+        /// Specifies how the size unit is displayed in the friendly name.
+        /// </summary>
+        public UnitDisplayMode UnitDisplayMode { get; set; }
+
+        /// <summary>
+        /// Represents the default options value for generating a friendly name.
+        /// </summary>
+        public static readonly FriendlyNameOptions Default = new FriendlyNameOptions();
+    }
+
+    /// <summary>
+    /// Specifies how the size unit (KB, MB, etc) is displayed in the friendly name.
+    /// </summary>
+    public enum UnitDisplayMode
+    {
+        /// <summary>
+        /// Always display the size unit (the default).
+        /// </summary>
+        AlwaysDisplay,
+
+        /// <summary>
+        /// Always hide the size unit.
+        /// </summary>
+        AlwaysHide,
+
+        /// <summary>
+        /// Only display the size unit if the value is 1 KB or more. Never display for sizes less than that.
+        /// </summary>
+        HideOnlyForBytes
+    }
+}
---
+++ sources/CodeBits_Net30/EnumIterator.cs
@@ -0,0 +1,61 @@
+﻿#region --- License & Copyright Notice ---
+/*
+CodeBits Code Snippets
+Copyright (c) 2012-2017 Jeevan James
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#endregion
+
+/* Documentation: https://github.com/JeevanJames/CodeBits/wiki/EnumIterator */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeBits
+{
+    /// <summary>
+    /// Provides iterators for enum types. Can be used in a LINQ expression.
+    /// </summary>
+    public static partial class EnumIterator
+    {
+        /// <summary>
+        /// Generates an iterator for the enum type specified by the TEnum generic parameter.
+        /// </summary>
+        /// <typeparam name="TEnum">The enum type to generate the iterator for</typeparam>
+        /// <returns>An generic iterator that can iterate over the values of TEnum</returns>
+        public static IEnumerable<TEnum> For<TEnum>()
+        {
+            if (!typeof(TEnum).IsEnum)
+                throw new ArgumentException("Generic parameter must be an enum");
+            return Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+        }
+
+        /// <summary>
+        /// Generates an iterator for the enum type specified by the TEnum generic parameter.
+        /// </summary>
+        /// <param name="enumType">The enum type to generate the iterator for</param>
+        /// <returns>A non-generic iterator that can iterate over the values of the enum</returns>
+        public static IEnumerable For(Type enumType)
+        {
+            if (enumType == null)
+                throw new ArgumentNullException("enumType");
+            if (!enumType.IsEnum)
+                throw new ArgumentException("enumType must be an enum");
+            return Enum.GetValues(enumType);
+        }
+    }
+}
